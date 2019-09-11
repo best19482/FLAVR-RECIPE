699e1920fa5f553819c6117dae0a0c5d67cf3e9b
Bankbest <abracadabra.best@gmail.com>
2019-09-11 10:35:28 +0700

    Create File

---
+++ FLAVR-RECIPE/FLAVR-RECIPE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FLAVR-RECIPE
+//
+//  Created by SD11 on 11/9/2562 BE.
+//  Copyright © 2562 SD11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
